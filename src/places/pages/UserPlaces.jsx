191edbabdfc31704411d90271221779450851433
Rki0 <pky00823@gmail.com>
2023-01-09 16:46:26 +0900

    client side task completed for Geo App

---
+++ src/places/pages/UserPlaces.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import { useParams } from "react-router-dom";
+
+import PlaceList from "../components/PlaceList";
+
+const DUMMY_PLACES = [
+  {
+    id: "p1",
+    title: "Empire State Building",
+    description: "One of the most famous sky scrpers",
+    imageUrl:
+      "https://lh5.googleusercontent.com/p/AF1QipNVlM5lo7fIJrmvjN4EOrTMiQjDgDyTfw7ATdV6=w408-h272-k-no",
+    address: "20 W 34th St., New York, NY 10001 미국",
+    locations: {
+      lat: 40.7484405,
+      lng: -73.9856644,
+    },
+    creator: "u1",
+  },
+  {
+    id: "p2",
+    title: "Emp. State Building",
+    description: "One of the most famous sky scrpers",
+    imageUrl:
+      "https://lh5.googleusercontent.com/p/AF1QipNVlM5lo7fIJrmvjN4EOrTMiQjDgDyTfw7ATdV6=w408-h272-k-no",
+    address: "20 W 34th St., New York, NY 10001 미국",
+    locations: {
+      lat: 40.7484405,
+      lng: -73.9856644,
+    },
+    creator: "u2",
+  },
+];
+
+function UserPlaces() {
+  const userId = useParams().userId;
+  const loadedPlaces = DUMMY_PLACES.filter((place) => place.creator === userId);
+
+  return <PlaceList items={loadedPlaces} />;
+}
+
+export default UserPlaces;
